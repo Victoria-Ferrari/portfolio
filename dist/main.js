184ec947b9756b703b3e60f8bdab269b45495f8d
Victoria <victoriaferrari@mohole.it>
2022-04-05 16:20:41 +0200

    git dont ignore dist

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("/* toggle menu */\r\nconst toggler = document.querySelector(\".menu__toggler\")\r\nconst menu = document.querySelector(\".menu\")\r\nconst arrow = document.querySelector(\".arrow\")\r\n\r\ntoggler.addEventListener(\"click\", () => {\r\n  toggler.classList.toggle(\"active\")\r\n  menu.classList.toggle(\"active\")\r\n  arrow.classList.toggle(\"reverse\")\r\n})\r\n\n\n//# sourceURL=webpack://portfolio/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	
+/******/ })()
+;
